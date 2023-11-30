cc9bd7f8d4bce10ad99045d69ea0ae347f35b410
tayyaba muslim <muslimtayyaba@gmail.com>
2023-11-30 10:59:38 -0800

    Quizz in(html css js)

---
+++ script1.js
@@ -0,0 +1,27 @@
+const startBtn = document.querySelector(".start-btn");
+const popupInfo = document.querySelector(".popup-info");
+const exitBtn = document.querySelector(".exit-btn");
+const main = document.querySelector(".main");
+const continueBtn = document.querySelector(".continue-btn");
+const quizSection = document.querySelector(".quiz-section");
+const quizBox = document.querySelector(".quiz-box");
+
+startBtn.onclick = () => {
+  popupInfo.classList.add("active");
+  main.classList.add("active");
+};
+
+exitBtn.onclick = () => {
+  popupInfo.classList.remove("active");
+  main.classList.remove("active");
+};
+
+continueBtn.onclick = () => {
+  quizSection.classList.add("active");
+  popupInfo.classList.remove("active");
+  main.classList.remove("active");
+  quizBox.classList.add("active");
+
+ 
+};
+
